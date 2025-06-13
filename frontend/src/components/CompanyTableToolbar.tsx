ff8d3d26c2b1556ee44ccb51ea9cac8500eda107
Josh Archibald <43184986+jsarchibald@users.noreply.github.com>
2025-06-12 22:24:09 -0400

    restructure

--- frontend/src/components/CompanyTableToolbar.tsx
+++ frontend/src/components/CompanyTableToolbar.tsx
@@ -1,8 +1,6 @@
 import { GridRowId, GridToolbarContainer } from "@mui/x-data-grid";
 import { useContext, useState } from "react";
 import {
-  addCompaniesToCollection,
-  checkBulkCompanyAdd,
   ICollection,
 } from "../utils/jam-api";
 import {
@@ -15,10 +13,10 @@ import {
   MenuItem,
   Stack,
 } from "@mui/material";
-import { SnackbarContext } from "../utils/contexts";
 import { NumberFormat } from "../utils/numbers";
+import CompanyToCollectionToolbarButton from "./CompanyToCollectionToolbarButton";
 
-const CompanyToCollectionToolbarButton = ({
+const CompanyTableToolbar = ({
   selectedIds,
   pageSize,
   total,
@@ -29,49 +27,6 @@ const CompanyToCollectionToolbarButton = ({
   total: number;
   collectionsList: ICollection[];
 }) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const snackbarContext = useContext(SnackbarContext);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const triggerBulkAdd = (
-    collection: ICollection,
-    selectedRows: readonly GridRowId[],
-  ) => {
-    // Start bulk operation
-    addCompaniesToCollection(collection.id, selectedRows.map(Number))
-      .then((response) => {
-        const companies_queued = response.companies_queued;
-        snackbarContext?.setOpen(true);
-        let message = `Adding ${companies_queued} compan${companies_queued == 1 ? "y" : "ies"} to ${collection.collection_name}.`;
-        if (response.companies_queued > 10)
-          message += " This might take a few minutes.";
-        snackbarContext?.setMessage(message);
-
-        let repeat = setInterval(() => {
-          checkBulkCompanyAdd(response.task_id).then((response) => {
-            if (response.status == "SUCCESS") {
-              snackbarContext?.setOpen(true);
-              let message = `Finished adding ${companies_queued} compan${companies_queued == 1 ? "y" : "ies"} to ${collection.collection_name}.`;
-              snackbarContext?.setMessage(message);
-
-              clearInterval(repeat);
-            }
-          });
-        }, 5000);
-      })
-      .catch((error) => {
-        console.log(error);
-        snackbarContext?.setOpen(true);
-        snackbarContext?.setMessage(error.response.data.detail);
-      });
-  };
-
-  console.log(pageSize, total, selectedIds.length);
 
   const selectAllAcrossPagesAlert = (
     <Box>
@@ -86,65 +41,24 @@ const CompanyToCollectionToolbarButton = ({
   );
 
   return (
-    <Stack
-      divider={<Divider orientation="horizontal" flexItem />}
-      alignItems={"stretch"}
-      direction={"column"}
-      sx={{ width: "100%" }}
-    >
-      <Box alignItems={"flex-start"} textAlign={"left"}>
-        <Button onClick={handleClick} disabled={selectedIds.length < 1}>
-          Add to collection
-        </Button>
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          slotProps={{
-            list: {
-              "aria-labelledby": "basic-button",
-            },
-          }}
-        >
-          {collectionsList.map((collection) => (
-            <MenuItem
-              key={`bulk_add_collection_menu_${collection.id}`}
-              onClick={() => {
-                triggerBulkAdd(collection, selectedIds);
-              }}
-            >
-              {collection.collection_name}
-            </MenuItem>
-          ))}
-        </Menu>
-      </Box>
-      {selectedIds.length == pageSize &&
+    <GridToolbarContainer>
+      <Stack
+        divider={<Divider orientation="horizontal" flexItem />}
+        alignItems={"stretch"}
+        direction={"column"}
+        sx={{ width: "100%" }}
+      >
+
+        <CompanyToCollectionToolbarButton
+          selectedIds={selectedIds}
+          pageSize={pageSize}
+          total={total}
+          collectionsList={collectionsList}
+        />
+        {selectedIds.length == pageSize &&
         selectedIds.length < total &&
         selectAllAcrossPagesAlert}
-    </Stack>
-  );
-};
-
-const CompanyTableToolbar = ({
-  selectedIds,
-  pageSize,
-  total,
-  collectionsList,
-}: {
-  selectedIds: readonly GridRowId[];
-  pageSize: number;
-  total: number;
-  collectionsList: ICollection[];
-}) => {
-  return (
-    <GridToolbarContainer>
-      <CompanyToCollectionToolbarButton
-        selectedIds={selectedIds}
-        pageSize={pageSize}
-        total={total}
-        collectionsList={collectionsList}
-      />
+      </Stack>
     </GridToolbarContainer>
   );
 };
---
+++ frontend/src/components/CompanyToCollectionToolbarButton.tsx
@@ -0,0 +1,104 @@
+import { GridRowId } from "@mui/x-data-grid";
+import { useContext, useState } from "react";
+import {
+  addCompaniesToCollection,
+  checkBulkCompanyAdd,
+  ICollection,
+} from "../utils/jam-api";
+import {
+  Box,
+  Button,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { SnackbarContext } from "../utils/contexts";
+
+const CompanyToCollectionToolbarButton = ({
+  selectedIds,
+  pageSize,
+  total,
+  collectionsList,
+}: {
+  selectedIds: readonly GridRowId[];
+  pageSize: number;
+  total: number;
+  collectionsList: ICollection[];
+}) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const snackbarContext = useContext(SnackbarContext);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const triggerBulkAdd = (
+    collection: ICollection,
+    selectedRows: readonly GridRowId[],
+  ) => {
+    // Start bulk operation
+    addCompaniesToCollection(collection.id, selectedRows.map(Number))
+      .then((response) => {
+        const companies_queued = response.companies_queued;
+        snackbarContext?.setOpen(true);
+        let message = `Adding ${companies_queued} compan${companies_queued == 1 ? "y" : "ies"} to ${collection.collection_name}.`;
+        if (response.companies_queued > 10)
+          message += " This might take a few minutes.";
+        snackbarContext?.setMessage(message);
+
+        let repeat = setInterval(() => {
+          checkBulkCompanyAdd(response.task_id).then((response) => {
+            if (response.status == "SUCCESS") {
+              snackbarContext?.setOpen(true);
+              let message = `Finished adding ${companies_queued} compan${companies_queued == 1 ? "y" : "ies"} to ${collection.collection_name}.`;
+              snackbarContext?.setMessage(message);
+
+              clearInterval(repeat);
+            }
+          });
+        }, 5000);
+      })
+      .catch((error) => {
+        console.log(error);
+        snackbarContext?.setOpen(true);
+        snackbarContext?.setMessage(error.response.data.detail);
+      });
+  };
+
+  console.log(pageSize, total, selectedIds.length);
+
+  return (
+    <Box alignItems={"flex-start"} textAlign={"left"}>
+        <Button onClick={handleClick} disabled={selectedIds.length < 1}>
+          Add to collection
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          slotProps={{
+            list: {
+              "aria-labelledby": "basic-button",
+            },
+          }}
+        >
+          {collectionsList.map((collection) => (
+            <MenuItem
+              key={`bulk_add_collection_menu_${collection.id}`}
+              onClick={() => {
+                triggerBulkAdd(collection, selectedIds);
+              }}
+            >
+              {collection.collection_name}
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
+      
+
+  );
+};
+
+export default CompanyToCollectionToolbarButton;
